7b5c2834ff69f31df9bec84565e1716ab56f8601
NenadGeorgievski <nenad.georgievski.ku@hotmail.com>
2022-10-13 00:03:40 +0200

    Add project files.

---
+++ Ex01_Table/Program.cs
@@ -0,0 +1,47 @@
+﻿using Ex01_Table;
+
+Table[] tableArr = new Table[10];
+
+
+for (int i = 0; i < 10; i++)
+{
+    try
+    {
+        Console.WriteLine("Enter the width of the table");
+        int tableWidth = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the height of the table:");
+        int tableHeight = Convert.ToInt32(Console.ReadLine());
+        if ((tableWidth > 50 && tableWidth < 200) && (tableHeight > 50 && tableHeight < 200))
+        {
+            Table newTable = new Table(tableWidth, tableHeight);
+            tableArr[i] = newTable;  
+        }
+        else
+        {
+            throw new ArgumentOutOfRangeException("The dimensions are out of range.");
+        }
+    }
+    catch (ArgumentOutOfRangeException ex)
+    {
+        Console.WriteLine($"{ex.Message}");
+        break; 
+    }
+}
+
+for (int i = 0; i < tableArr.Length; i++)
+{
+    if (tableArr[i] == null)
+    {
+        Console.WriteLine("You did not input the correct format. This script will now terminate.");
+        break;
+    } else
+    {
+        tableArr[i].showData();
+    }
+}
+
+
+
+
+
+
---
+++ Ex01_Table/Table.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01_Table
+{
+    public class Table
+    {
+        public int Width;
+        public int Height;
+
+        public Table(int width, int height)        {
+            Width = width;
+            Height = height;
+        }
+
+        public void showData()
+        {
+            Console.WriteLine($"The width of the table is {Width}, and the height of the table is {Height}. " );
+        }
+
+    }
+}
