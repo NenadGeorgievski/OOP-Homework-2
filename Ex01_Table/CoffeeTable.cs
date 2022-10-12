a219905ae793bad42e35feb633d5452b79f04f01
NenadGeorgievski <nenad.georgievski.ku@hotmail.com>
2022-10-13 00:27:57 +0200

    Added CoffeeTable class to Homework exercises 1 and 3 combined.

---
+++ Ex01_Table/CoffeeTable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01_Table
+{
+    public class CoffeeTable : Table
+    {
+        public int coffeeTableHeight;
+        public int coffeeTableWidth;
+        public CoffeeTable(int width, int height) : base(width, height)
+        {
+            coffeeTableHeight = height; 
+            coffeeTableWidth = width;
+        }
+
+        public void showData()
+        {
+            Console.WriteLine($"Coffee table: width is {Width}, and the height is {Height}");
+        }
+    }
+}
--- Ex01_Table/Program.cs
+++ Ex01_Table/Program.cs
@@ -40,8 +40,83 @@ for (int i = 0; i < tableArr.Length; i++)
     }
 }
 
+Table[] mixedArr = new Table[10];
+for (int i = 0; i < 5; i++)
+{
+    try
+    {
+        Console.WriteLine("Enter the width of the table");
+        int tableWidth = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the height of the table:");
+        int tableHeight = Convert.ToInt32(Console.ReadLine());
+        if ((tableWidth > 50 && tableWidth < 200) && (tableHeight > 50 && tableHeight < 200))
+        {
+            Table newTable = new Table(tableWidth, tableHeight);
+            mixedArr[i] = newTable;
+        }
+        else
+        {
+            throw new ArgumentOutOfRangeException("The dimensions are out of range.");
+        }
+    }
+    catch (ArgumentOutOfRangeException ex)
+    {
+        Console.WriteLine($"{ex.Message}");
+        break;
+    }
+}
+
+for (int i = 5; i < 10; i++)
+{
+    try
+    {
+        Console.WriteLine("Enter the width of the coffee table");
+        int coffeeTableWidth = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the height of the coffee table:");
+        int coffeeTableHeight = Convert.ToInt32(Console.ReadLine());
+        if ((coffeeTableWidth > 40 && coffeeTableWidth < 120) && (coffeeTableHeight > 40 && coffeeTableHeight < 120))
+        {
+            Table newTable = new CoffeeTable(coffeeTableWidth, coffeeTableHeight);
+            mixedArr[i] = newTable;
+        }
+        else
+        {
+            throw new ArgumentOutOfRangeException("The dimensions are out of range.");
+        }
+    }
+    catch (ArgumentOutOfRangeException ex)
+    {
+        Console.WriteLine($"{ex.Message}");
+        break;
+    }
+}
 
 
+for (int i = 0; i < 5; i++)
+{
+    if (mixedArr[i] == null)
+    {
+        Console.WriteLine("You did not input the correct format. This script will now terminate.");
+        break;
+    }
+    else
+    {
+        mixedArr[i].showData();
+    }
+}
+
+for (int i = 5; i < 10; i++)
+{
+    if (mixedArr[i] == null)
+    {
+        Console.WriteLine("You did not input the correct format. This script will now terminate.");
+        break;
+    }
+    else
+    {
+        ((CoffeeTable)mixedArr[i]).showData();
+    }
+}
 
 
 
